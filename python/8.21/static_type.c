47216a91513ca1a4a75b47a79c7bf915a266e777
yangzaza3319 <ljy3192003@163.com>
2025-08-22 13:49:17 +0800

    8.22续8.21基础2

--- python/8.21/static_type.c
+++ python/8.21/static_type.c
@@ -2,17 +2,17 @@
 
 int main()
 {
-    int a = 10;        // 整数类型
-    printf("%d\n", a); // 输出: 10
+    int a = 10;           // 整数类型
+    printf("%d\n", a);   // 输出：10
 
-    a = "Hello";       // 现在是字符串类型
-    printf("%s\n", a); // 输出: Hello
+    char *str = "Hello"; // 字符串类型
+    printf("%s\n", str); // 正确的输出：Hello
 
-    float b = 3.14;    // 浮点数类型
-    printf("%f\n", b); // 输出: 3.140000
+    float b = 3.14;       // 浮点数类型
+    printf("%f\n", b);   // 输出：3.140000
 
-    char c = 'A';      // 字符类型
-    printf("%c\n", c); // 输出: A
+    char c = 'A';        // 字符类型
+    printf("%c\n", c);   // 输出：A
 
     return 0;
 }
