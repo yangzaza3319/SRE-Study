a1ff40ff41a0ea8e4734ead3862c2974cbba7073
yangzaza3319 <ljy3192003@163.com>
2025-09-24 14:37:34 +0800

    9.24补充装饰器笔记以及示例代码

--- python/9.23/declarators_example.py
+++ python/9.23/declarators_example.py
@@ -147,7 +147,7 @@ inner
 from functools import wraps
 import time
 
-def fun1():
+def func1():
     print('in func1')
 
 def timer(func):
@@ -159,3 +159,165 @@ def timer(func):
     return innner
 func1 = timer(func1)
 print(func1.__name__)
+"""
+输出
+func1
+"""
+
+# 7 带参数的装饰器
+## 案例：创建一个装饰器，接受一个参数，用于指定是否打印函数的执行时间
+
+import time
+from functools import wraps
+
+def timing_decorator(print_time=True):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args,**kwargs):
+            start_time = time.time()
+            result = func(*args,**kwargs)
+            end_time = time.time()
+            if print_time:
+                execution_time = end_time - start_time
+                print(f"{func.__name__} 执行时间：{execution_time:.4f}秒")
+            return result
+        return wrapper
+    return decorator
+@timing_decorator(print_time=True)
+def add(x,y):
+    time.sleep(1)
+    return(x+y)
+
+@timing_decorator(print_time=False)
+def multiply(x,y):
+    time.sleep(1)
+    return x * y
+
+result_add = add(3,6)
+print(f"加法结果为：{result_add}")
+result_mutiply = multiply(6,7)
+print(f"乘法结果为：{result_mutiply}")
+"""
+    输出结果
+print(f"加法结果为：{result_add}")
+result_mutiply = multiply(6,7)
+print(f"乘法结果为：{result_mutiply}")
+add 执行时间：1.0070秒
+>>> print(f"加法结果为：{result_add}")
+加法结果为：9
+>>> result_mutiply = multiply(6,7)
+>>> print(f"乘法结果为：{result_mutiply}")
+乘法结果为：42
+"""
+
+# 8 多个装饰器装饰同一个函数
+
+def decorator1(func):
+    print("decorator1 被应用")
+    def wrapper1():
+        print("进入 decorator1")
+        func()  # ← 这里的 func 是被它包装的函数
+        print("退出 decorator1")
+    return wrapper1
+
+def decorator2(func):
+    print("decorator2 被应用")
+    def wrapper2():
+        print("进入 decorator2")
+        func()  # ← 这里的 func 是被它包装的函数
+        print("退出 decorator2")
+    return wrapper2
+
+@decorator1
+@decorator2
+def f():
+    print("hello")
+
+f()
+"""
+    输出
+decorator2 被应用
+decorator1 被应用
+>>> f()
+进入 decorator1
+进入 decorator2
+hello
+退出 decorator2
+退出 decorator1
+
+
+    程序执行的过程
+f = decorator2(f) -> func = f,return inner2
+f = decorator1(f) -> func = inner2,return inner1
+f() = inner1() -> inner2() -> f() -> inner2() -> inner1()
+
+       f()
+        ↓
+   [decorator1]   ← 最外层
+        ↓
+   [decorator2]   ← 中间层
+        ↓
+     原始函数     ← 最内层
+"""
+
+# 8.1 两个装饰器（一个打印函数执行时间、另一个打印调用参数）
+import time
+from functools import wraps
+
+def decorator_time(func):
+    print("这是打印函数执行时间的装饰器")
+    @wraps(func)
+    def wrapper(*args,**kwargs):
+        start = time.time()
+        result1 = func(*args,**kwargs)
+        end = time.time()
+        execution_time = end - start
+        print(f"{func.__name__}执行时间：{execution_time:.4f}秒")
+        return result1
+    return wrapper
+def decorator_argsPrint(func):
+    @wraps(func)
+    def wrapper(*args,**kwargs):
+        print(f"调用{func.__name__}函数，位置参数：{args}，关键字参数：{kwargs}")
+        result2 = func(*args,**kwargs)
+        return result2
+    return wrapper
+
+@decorator_time
+@decorator_argsPrint
+def add(x,y,z):
+    time.sleep(1)
+    print(z)
+    return x + y ,z
+
+result1 = add(5,3,"返回元组")  # 参数个数需要对应
+print(f"加法结果：{result1}")
+"""
+输出
+这是打印函数执行时间的装饰器
+>>> result1 = add(5,3,"返回元组")  # 参数个数需要对应
+调用add函数，位置参数：(5, 3, '返回元组')，关键字参数：{}
+print(f"加法结果：{result1}")
+返回元组
+add执行时间：1.0036秒
+>>> print(f"加法结果：{result1}")
+加法结果：(8, '返回元组')
+"""
+
+# 10装饰器的固定结构
+from functools import wraps
+
+def decorator_with_args(param):
+    def actual_decorator(func):
+        @wraps(func)
+        def wrapper(*args,**kwargs):
+            print(f"装饰器参数:{param}")
+            return func(*args,**kwargs)
+        return wrapper
+    return actual_decorator
+
+@decorator_with_args("配置参数")
+def my_function():
+    print("my_function 被调用")
+
+my_function()
