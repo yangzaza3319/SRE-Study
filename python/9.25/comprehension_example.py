03fc1e0897372e033a0453e9f3862399ccc34edb
jeyng <ljy3192003@163.com>
2025-10-10 10:36:56 +0800

    提交9.25推导式（列表推导式和集合推导式）

--- python/9.25/comprehension_example.py
+++ python/9.25/comprehension_example.py
@@ -16,10 +16,26 @@ print(even_numbers)  # 输出 [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 fruits = [['peach','lemon','pear','avocado','cantaloupe','banana','grape'],
           ['raisins','plum','apricot','nectarine','orange','papaya']]
 
-print([name for list in fruits for name in list if name.count('a') >= 2])
+print([name for list in fruits for name in list if name.count('a') >= 2]) # 输出 ['avocado', 'cantaloupe', 'banana', 'papaya']
 
 
 # 推导式案例2. 找到30以内所有能被3整除的数
 
 mutiples = [i for i in range(30) if i % 3 == 0]
 print(mutiples)   # 输出 [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+
+
+# 集合推导式案例1: 输出0～3数字的平方
+squares_set = {x ** 2 for x in range(0,4)}
+print(squares_set)  # 输出{0,1,4,9}
+
+# 集合推导式案例2:字符串导出为字符集合
+unique_chars = {char for char in "hello,word!"}
+print(unique_chars)  # 输出 {'e', ',', 'o', 'r', 'd', 'h', 'l', '!', 'w'}
+
+# 集合推导式案例3:计算列表中每个值的平方
+l = [1,2,3,-2,-1,-3]
+
+squares = {x**2 for x in l}
+print(squares) # 输出 {1, 4, 9}，自带去重功能
+
